52d06872abfd4af981dd4bd1545ff621deb794b2
minipingu <ranmaniax@gmail.com>
2025-02-18 14:43:34 +0700

    add tailwind

---
+++ nextjs-yarn/app/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Title } from '@mantine/core'
+
+export default function Home() {
+	return (
+		<Box>
+			<Title order={3} className='text-3xl font-bold underline'>
+				This is TITLE
+			</Title>
+		</Box>
+	)
+}
